ee587c4fb73c23424e73ae5ee0203ad90479ab6d
German Rodriguez <german@decemberlabs.com>
2018-10-10 17:35:39 -0300

    Add book rating modal

--- amazon2/static/libs/custom/js/custom.js
+++ amazon2/static/libs/custom/js/custom.js
@@ -1,14 +1,38 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget)
-    var recipient = button.data('whatever') 
-  })
-
-$(document).ready(function(e){
-  $('.search-panel .dropdown-menu').find('a').click(function(e) {
-      e.preventDefault();
-  var param = $(this).attr("href").replace("#","");
-  var concept = $(this).text();
-  $('.search-panel span#search_concept').text(concept);
-  $('.input-group #search_param').val(param);
-	});
+$("#exampleModal").on("show.bs.modal", function(event) {
+  var button = $(event.relatedTarget);
+  var recipient = button.data("whatever");
 });
+
+document.querySelectorAll(".star").forEach(element =>
+  element.addEventListener("click", function(event) {
+    const id = event.srcElement.id;
+    const number = parseInt(id[id.length - 1]);
+    for (i = 1; i <= 5; i++) {
+      let path = "../icons/";
+      path += i <= number ? "star_fill.png" : "star_empty.png";
+      const star = document.querySelector(`#star${i}`);
+      star.src = path;
+      star.classList += i <= number ? "filled" : "empty";
+    }
+    document.querySelector("#rate").disabled = false;
+  })
+);
+
+function rateBook() {
+  const text = document.querySelector("textarea").value;
+  if (getStars()) {
+    console.log("error");
+  } else {
+    alert("success");
+  }
+}
+
+function getStars() {
+  let stars = 0;
+  document.querySelectorAll(".star").forEach(function(element) {
+    if (element.classList.contains("filled")) {
+      stars += 1;
+    }
+  });
+  return stars;
+}
